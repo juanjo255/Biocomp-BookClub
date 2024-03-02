a5181ecfcbaf2a1fb9bd96cfc5410359d11621d0
juanjo255 <piconcossio@gmail.com>
2024-03-02 11:59:57 -0500

    last problem: 1H

--- BioinformaticsAlgorithms/chapter1/rust/ori_finder/src/main.rs
+++ BioinformaticsAlgorithms/chapter1/rust/ori_finder/src/main.rs
@@ -2,10 +2,11 @@ pub mod utils;
 
 fn main() {
     println!("Ori finder!");
-    utils::frequent_words(
-        String::from("gatcagcataagggtccCTGCAATGCATGACAAGCCTGCAGTtgttttac"),
-        4, 25, 3 
-    )
+    // utils::frequent_words(
+    //     String::from("gatcagcataagggtccCTGCAATGCATGACAAGCCTGCAGTtgttttac"),
+    //     4, 25, 3 
+    // )
     //utils::find_clumps("atcaatgatcaacgtaagcttctaagcatg", "atc", 4, 3)
-
+    //utils::minimum_skew(String::from("CATGGGCATCGGCCATACGCC"));
+    utils::approx_pattern_match("AAAAA",String::from("AACAAGCATAAACATTAAAGAG"), 1);
 }
--- BioinformaticsAlgorithms/chapter1/rust/ori_finder/src/utils.rs
+++ BioinformaticsAlgorithms/chapter1/rust/ori_finder/src/utils.rs
@@ -1,4 +1,4 @@
-use std::cmp::min;
+use std::{cmp::min, iter::zip};
 
 pub fn pattern_count(text: &str, pattern: &str) -> (u64, Vec<u64>) {
     // Count how many time a given patter appears in text and positions in text
@@ -75,8 +75,8 @@ pub fn frequent_words(text: String, k: u8, l: u8, t: u64) {
 
             // Count instances of pattern
             let count_pos = pattern_count(&text, &pattern);
-             
-            count.push(count_pos.clone());            
+
+            count.push(count_pos.clone());
 
             // Check if pattern forms a clump and if it's not already in the group
             if (count_pos.0 >= t)
@@ -94,11 +94,13 @@ pub fn frequent_words(text: String, k: u8, l: u8, t: u64) {
     let max_num = count.iter().map(|x| x.0).max().unwrap().to_owned();
     println!("max_num: {max_num}");
     for i in 0..=(text.len() - k as usize) {
-        if count[i].0 == max_num && !(frequent_pattern
-          .iter()
-          .map(|x| x.0)
-          .collect::<Vec<&str>>()
-          .contains(&&text[i..i + (k as usize)]))  {
+        if count[i].0 == max_num
+            && !(frequent_pattern
+                .iter()
+                .map(|x| x.0)
+                .collect::<Vec<&str>>()
+                .contains(&&text[i..i + (k as usize)]))
+        {
             frequent_pattern.push((&text[i..i + (k as usize)], count[i].1.clone()));
         }
     }
@@ -107,16 +109,66 @@ pub fn frequent_words(text: String, k: u8, l: u8, t: u64) {
     println!("Clumps: {:?}", clumps);
 }
 
-// pub fn find_clumps (text:&str, pattern:&str, w:usize, t:u8){
-//   clumps
-//   // take chuncks and look for k-mer clumps (number of k-mers in w equal or greater than t).
-//   for i in 0..=(text.len() - w){
-//     let chunk = &text[i..i + w];
-//     for j in 0..=(chunk.len() - pattern.len()){
-//       let pattern = &chunk[j..j+pattern.len()];
-//       if pattern_count(&text, pattern) > t{
+// Problem 1F
+// Find a position in a genome where the skew diagram attains a minimum.
+pub fn minimum_skew(dna: String) -> i8 {
+    let mut skew_values: Vec<i8> = Vec::new();
+    for nucl in dna.chars() {
+        match nucl {
+            'G' => skew_values.push(match skew_values.last() {
+                Some(i) => i + 1,
+                None => 1,
+            }),
+            'C' => skew_values.push(match skew_values.last() {
+                Some(i) => i - 1,
+                None => -1,
+            }),
+            _ => skew_values.push((skew_values.last().unwrap_or(&0)).to_owned()),
+        }
+    }
+    return skew_values.iter().min().unwrap().to_owned();
+}
 
-//       }
-//     }
-//   }
-// }
+// Problem 1G
+// Compute the Hamming distance between two strings.
+pub fn hamming_distance(pattern: &str, pattern2: &str) -> i32 {
+    let mut score = 0;
+    let pattern_iter = pattern.chars();
+    let pattern2_iter = pattern2.chars();
+    for (char1, char2) in zip(pattern_iter, pattern2_iter) {
+        if char1 != char2 {
+            score += 1;
+        }
+    }
+    //println!("Hamming distance: {score}");
+    return score;
+}
+
+// Problem 1H
+// Find all approximate occurrences of a pattern in a string.
+pub fn approx_pattern_match(pattern: &str, text: String, d: i32) -> (u64, Vec<u64>) {
+    // Counter for each time an instance of "pattern" is found
+    let mut count: u64 = 0;
+
+    // postions in text where pattern is found
+    let mut pattern_pos: Vec<u64> = Vec::new();
+
+    // Pattern size (k-mer)
+    let k = pattern.len();
+
+    // Reverse complement
+    let rev_compl_pattern = reverse_complement(pattern);
+
+    // Iterate for each k-mer in text
+    for i in 0..=(text.len() - k) {
+        let pattern_2 = &text[i..i + k];
+        if hamming_distance(pattern_2, pattern) <= d
+            || hamming_distance(pattern_2, &rev_compl_pattern) <= d
+        {
+            count += 1;
+            pattern_pos.push(i as u64);
+        }
+    }
+    println!("Instances found: {count}");
+    return (count, pattern_pos)
+}
